e91e5eef8decabc05d34ccd9e1e5bffd8312367d
WesterfieldCRL <anastasicrl@gmail.com>
2023-10-09 10:03:50 -0500

    added documentation

--- Program2/graph.h
+++ Program2/graph.h
@@ -1,3 +1,14 @@
+/* CSI 3334
+ * Programming Assignment 2
+ * Filename: graph.h
+ * Student name: Wesley Anastasi
+ * version: 1.0
+ * 
+ * This file contains the header for the graph class. The graph class is used
+ * to represent the relationships between members of a club. The graph class
+ * uses an adjacency list to represent the graph.
+ */
+
 #ifndef GRAPH_H
 #define GRAPH_H
 
@@ -32,20 +43,69 @@ private:
     map<T, std::vector<T>> adjacencyList;
 };
 
+/**
+ * Graph
+ *
+ * This function reads in a list of members and the languages they speak and
+ * understand from a file and returns a vector of members.
+ * 
+ * Parameters:
+ *   none
+ * 
+ * Return value: none
+ */
+
 template<typename T>
 Graph<T>::Graph() {
     adjacencyList = map<T, vector<T>>();
 }
+
+/**
+ * addVertex
+ *
+ * This function reads in a list of members and the languages they speak and
+ * understand from a file and returns a vector of members.
+ * 
+ * Parameters:
+ *   T: the vertex to add
+ * 
+ * Return value: none
+ */
 
 template<typename T>
 void Graph<T>::addVertex(T vertex) {
     adjacencyList[vertex] = vector<T>();
 }
 
+/**
+ * addEdge
+ *
+ * This function reads in a list of members and the languages they speak and
+ * understand from a file and returns a vector of members.
+ * 
+ * Parameters:
+ *   from: the vertex to add the edge from
+ *   to: the vertex to add the edge to
+ * 
+ * Return value: none
+ */
+
 template<typename T>
 void Graph<T>::addEdge(T from, T to) {
     adjacencyList[from].push_back(to);
 }
+
+/**
+ * printGraph
+ *
+ * This function reads in a list of members and the languages they speak and
+ * understand from a file and returns a vector of members.
+ * 
+ * Parameters:
+ *   out: the output stream to print to
+ * 
+ * Return value: none
+ */
 
 template<typename T>
 void Graph<T>::printGraph(ostream &out) {
@@ -57,6 +117,19 @@ void Graph<T>::printGraph(ostream &out) {
         out << endl;
     }
 }
+
+/**
+ * isPath
+ *
+ * This function reads in a list of members and the languages they speak and
+ * understand from a file and returns a vector of members.
+ * 
+ * Parameters:
+ *   from: the vertex to start from
+ *   to: the vertex to end at
+ * 
+ * Return value: true if there is a path from "from" to "to", false otherwise
+ */
 
 template<typename T>
 bool Graph<T>::isPath(T from, T to) {
